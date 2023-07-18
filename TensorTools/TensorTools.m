cc3466ee0d55622fe1cbcb7419e253eb9860afa2
Ross Dempsey <rossd79@gmail.com>
2023-07-18 12:21:14 -0400

    add SymmetryReduce

--- TensorTools/TensorTools.m
+++ TensorTools/TensorTools.m
@@ -275,6 +275,47 @@ SymmetryPermutations[symmetry_, OptionsPattern[]] := symmetry /. If[!TrueQ[Optio
    }];
 
 DeclareTensorSymmetry[name_, symmetry_] := (TensorSymmetries[name] = symmetry); 
+
+SymmetryReduce[t_Tensor] := t;
+SymmetryReduce[Contract[t_Tensor, pairs_]] := Contract[t, pairs];
+SymmetryReduce[Contract[TensorPermute[t_, perm_], pairs_]] := 
+  Module[{symb, ninds, generators, rules, allperms, minimal},
+   symb = Symbolic[t];
+   ninds = Total[Length /@ symb - 1];
+   generators = 
+    Flatten[Table[
+      TensorSymmetries[symb[[i, 1]]] /. 
+       Cycles[xs_] :> 
+        Cycles[xs + Total[Length /@ symb[[;; i - 1]] - 1]], {i, 
+       Length[symb]}], 1];
+   rules = Join[Rule @@@ pairs, Rule @@@ (Reverse /@ pairs)];
+   generators = 
+    Join[generators, 
+     Cases[generators, {Cycles[xs_], sign_} /; 
+        AllTrue[Flatten[xs], MemberQ[Keys[rules], #] &] :> {Cycles[
+         xs /. rules], sign}]];
+   allperms = SymmetryPermutations[generators];
+   minimal = 
+    MinimalBy[allperms, perm[[PermutationList[#[[1]], ninds]]] &][[1]];
+   minimal[[2]] Contract[
+     TensorPermute[t, perm[[PermutationList[minimal[[1]], ninds]]]], 
+     pairs]
+   ];
+SymmetryReduce[TensorPermute[t_, perm_]] := 
+  Module[{symb, ninds, generators, allperms, minimal},
+   symb = Symbolic[t];
+   ninds = Total[Length /@ symb - 1];
+   generators = 
+    Flatten[Table[
+      TensorSymmetries[symb[[i, 1]]] /. 
+       Cycles[xs_] :> 
+        Cycles[xs + Total[Length /@ symb[[;; i - 1]] - 1]], {i, 
+       Length[symb]}], 1];
+   allperms = SymmetryPermutations[generators];
+   minimal = 
+    MinimalBy[allperms, perm[[PermutationList[#[[1]], ninds]]] &][[1]];
+   minimal[[2]] TensorPermute[t, 
+     perm[[PermutationList[minimal[[1]], ninds]]]]];
    
 BuildTensor[{names : (_List)..}] := 
   TensorProduct @@ (BuildTensor /@ {names});
